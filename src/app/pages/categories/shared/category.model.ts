dd213da7e92a121a54dacec1fd0389f7d2725617
luiz <luiz>
2022-06-16 14:18:28 -0300

    Adicionando model base

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,7 +1,10 @@
-export class Category {
+import { BaseResourceModel } from "src/app/shared/moldes/base-resource.model";
+export class Category extends BaseResourceModel {
     constructor(
-        public id?:number,
+        public id?: number,
         public name?: string,
         public description?: string
-    ){}
+    ) {
+        super();
+    }
 }
